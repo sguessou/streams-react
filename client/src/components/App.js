a291bc6167412bc0cf328d265a91692bd54f315d
Saad Guessous <saad.guessous@gofore.com>
2019-09-17 20:20:34 +0300

    added stream object reducer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import StreamCreate from './streams/StreamCreate';
-import StreamEdit from './streams/StreamEdit';
-import StreamList from './streams/StreamList';
-import StreamShow from './streams/StreamShow';
-import StreamDelete from './streams/StreamDelete';
-import Header from './Header';
+import React from "react";
+import {Router, Route} from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamDelete from "./streams/StreamDelete";
+import Header from "./Header";
+import history from "../history";
 
 const App = () => {
-    return (
-        <div className="ui container">
-            <BrowserRouter>
-                <Header />
-                <div>
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route
-                        path="/streams/delete"
-                        exact
-                        component={StreamDelete}
-                    />
-                    <Route path="/streams/show" exact component={StreamShow} />
-                </div>
-            </BrowserRouter>
+  return (
+    <div className="ui container">
+      <Router history={history}>
+        <Header />
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
-    );
+      </Router>
+    </div>
+  );
 };
 
 export default App;
